49732aed3b1405e63ca6744893060aa219c2a8a6
nikololiahim <olomishcak@outlook.com>
2022-02-08 00:01:04 +0300

    created a simple web app with several components

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,18 +1,62 @@
 import japgolly.scalajs.react.*
 import japgolly.scalajs.react.vdom.html_<^.*
 import org.scalajs.dom
+import scala.util.Try
 
 
 object Main:
   def main(args: Array[String]): Unit =
 
-    val HelloMessage = ScalaComponent
-      .builder[String]
-      .render($ => <.div("Hello ", $.props))
+    case class FibProps(n: Int)
+    class Backend($: BackendScope[Unit, FibProps]):
+
+      def onTextChange(e: ReactEventFromInput) =
+        e.extract(_.target.value)(
+          value => $.modState(_ => Try(value.toInt)
+            .map(FibProps(_))
+            .getOrElse(FibProps(0)))
+        )
+
+      def fib(n: Int): BigInt =
+        (2 until n).foldLeft[(BigInt, BigInt)]((1, 1)) {
+          case ((n1, n2), _) => (n2, n1+n2)
+        }._2
+
+
+    val Input = ScalaComponent
+      .builder[(FibProps, Backend)]
+      .render_P {case (n, b) => <.input.text(
+        ^.value := n.n,
+        ^.onChange ==> b.onTextChange
+      )}
       .build
 
+
+    val Fibonacci = ScalaComponent
+      .builder[(FibProps, Backend)]
+      .render_P {
+        case (p, b) => <.div(
+          <.h1(s"${p.n}th fibonacci number is ${b.fib(p.n)}")
+        )
+      }
+      .build
+
+    val Page = ScalaComponent
+      .builder[Unit]
+      .initialState(FibProps(0))
+      .backend(new Backend(_))
+      .renderS(($, s) =>
+        <.div(
+          <.div("Enter desired fib index:"),
+          Input((s, $.backend)),
+          Fibonacci((s, $.backend)),
+        )
+      )
+      .build
+
+
     val root = dom.document.getElementById("app")
-    HelloMessage("JOHN").renderIntoDOM(root)
-    
+    Page().renderIntoDOM(root)
+
   end main
 end Main
