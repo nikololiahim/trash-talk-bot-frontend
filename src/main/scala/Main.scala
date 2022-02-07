cb433dcf4c8c04b8b6cb802eda695396e181e0cc
nikololiahim <olomishcak@outlook.com>
2022-02-07 21:34:57 +0300

    bringing back new syntax rules

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,11 @@
-import japgolly.scalajs.react._
-import japgolly.scalajs.react.vdom.html_<^._
+import japgolly.scalajs.react.*
+import japgolly.scalajs.react.vdom.html_<^.*
 import org.scalajs.dom
 
-object Main {
-  def main(args: Array[String]): Unit = {
+
+object Main:
+  def main(args: Array[String]): Unit =
+
     val HelloMessage = ScalaComponent
       .builder[String]
       .render($ => <.div("Hello ", $.props))
@@ -11,5 +13,6 @@ object Main {
 
     val root = dom.document.getElementById("app")
     HelloMessage("JOHN").renderIntoDOM(root)
-  }
-}
+    
+  end main
+end Main
